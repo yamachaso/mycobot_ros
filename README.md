37e898dfa8940bc14455f48914a164a6fb5eaa28
Piroyama <tissueloose@gmail.com>
2022-05-14 19:55:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# mycobot_moveit
+Unofficial myCobot moveit repositiry
+
+# Environment
+| item |  |
+|---|---|
+| OS | Ubuntu 18.04 |
+| ROS | melodic |
+| Arm Robot | myCobot |
+
+# Preparation
+1. Install pymycobot. See how to install it. https://github.com/elephantrobotics/pymycobot (AtomMain2.5 + pymycobot v2.2.0, or AtomMain2.4 + pymycobot v2.1.2)
+
+2. Build ROS packages. mycobot_moveit needs [Tiryoh](https://github.com/Tiryoh/mycobot_ros)'s repository (Thank you!).
+```
+cd catkin_ws/src
+git clone https://github.com/Tiryoh/mycobot_ros
+git clone https://github.com/nisshan-x/mycobot_moveit
+rosdep update
+rosdep install -i --from-paths mycobot_moveit
+cd ..
+catkin_make
+```
+
+# Usage
+Connect myCobot and PC with USB-C cable, and launch:
+
+`roslaunch mycobot_moveit mycobot_moveit_control.launch`
+
+Note: Don't forget to provide power via AC adaptor to myCobot.
+
+# License
+This software is released under the MIT License, see LICENSE.txt.
---
+++ scripts/mycobot_hw_interface.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2
+import subprocess
+import rospy
+import rosparam
+
+from std_msgs.msg import Float32MultiArray
+from pymycobot.mycobot import MyCobot
+from pymycobot.genre import Angle
+
+class MycobotHwInterface:
+    def __init__(self):
+        port_str = rospy.get_param("/hardware_interface/mycobot_port", "default")
+        if port_str == "default":
+            port = subprocess.run(['echo -n /dev/ttyUSB*'], shell=True, stdout=subprocess.PIPE, check=True).stdout.decode()
+        else:
+            port = subprocess.run(['echo -n ' + port_str], shell=True, stdout=subprocess.PIPE, check=True).stdout.decode()
+
+        self.mycobot_ = MyCobot(port)
+        self.mycobot_.power_on()
+
+        rospy.init_node('mycobot_hw_interface', anonymous=True)
+        self.joint_state_msg_pub_ = rospy.Publisher('joint_states_array', Float32MultiArray, queue_size=10)
+        self.joint_cmd_sub = rospy.Subscriber("joint_cmd_array", Float32MultiArray, self.jointCommandCallback)
+
+        self.rate_ = rospy.Rate(10) # 10hz
+
+        self.joint_state_array_ = []
+        JOINT_NUMBER = 6
+        for tmp in range(JOINT_NUMBER):  # initialize array
+            tmp = 0.0
+            self.joint_state_array_.append(tmp)
+        self.pre_data_list = []
+        self.first_flag = True
+
+    def main_loop(self):
+        while not rospy.is_shutdown():
+            self.joint_state_msg_sender()
+            self.rate_.sleep()
+
+    def joint_state_msg_sender(self):
+        angles = self.mycobot_.get_radians()
+        for index, value in enumerate(angles):
+            if index != 2:
+                value *= -1
+            self.joint_state_array_[index] = value
+        # str = "angles: %s" % angles
+        # rospy.loginfo(str)
+
+        joint_state_msg = Float32MultiArray(data=self.joint_state_array_)
+        self.joint_state_msg_pub_.publish(joint_state_msg)
+
+    def jointCommandCallback(self, msg):
+        data_list = []
+        for index, value in enumerate(msg.data):
+            if index != 2:
+                value *= -1
+            data_list.append(value)
+
+        if self.first_flag:
+            for value in data_list:
+                self.pre_data_list.append(value)
+            self.first_flag = False
+
+        if self.pre_data_list != data_list:
+            rospy.loginfo(rospy.get_caller_id() + "%s", msg.data)
+            self.mycobot_.send_radians(data_list, 80)
+            self.pre_data_list = []
+            for value in data_list:
+                self.pre_data_list.append(value)
+
+if __name__ == '__main__':
+    try:
+        mc_hw_if = MycobotHwInterface()
+        mc_hw_if.main_loop()
+    except rospy.ROSInterruptException:
+        pass
+
