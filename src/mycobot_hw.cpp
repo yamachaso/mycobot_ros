c7547dabd9bab46b710d205947441e79bf85f1ca
Piroyama <tissueloose@gmail.com>
2022-05-15 19:13:09 +0900

    update urdf

--- src/mycobot_hw.cpp
+++ src/mycobot_hw.cpp
@@ -2,31 +2,31 @@
 
 MyRobot::MyRobot(): nh_(""), private_nh_("~")
 {
-  hardware_interface::JointStateHandle state_handle_1("arm1_joint", &pos_[0], &vel_[0], &eff_[0]);
+  hardware_interface::JointStateHandle state_handle_1("joint1", &pos_[0], &vel_[0], &eff_[0]);
   jnt_state_interface.registerHandle(state_handle_1);
-  hardware_interface::JointStateHandle state_handle_2("arm2_joint", &pos_[1], &vel_[1], &eff_[1]);
+  hardware_interface::JointStateHandle state_handle_2("joint2", &pos_[1], &vel_[1], &eff_[1]);
   jnt_state_interface.registerHandle(state_handle_2);
-  hardware_interface::JointStateHandle state_handle_3("arm3_joint", &pos_[2], &vel_[2], &eff_[2]);
+  hardware_interface::JointStateHandle state_handle_3("joint3", &pos_[2], &vel_[2], &eff_[2]);
   jnt_state_interface.registerHandle(state_handle_3);
-  hardware_interface::JointStateHandle state_handle_4("arm4_joint", &pos_[3], &vel_[3], &eff_[3]);
+  hardware_interface::JointStateHandle state_handle_4("joint4", &pos_[3], &vel_[3], &eff_[3]);
   jnt_state_interface.registerHandle(state_handle_4);
-  hardware_interface::JointStateHandle state_handle_5("arm5_joint", &pos_[4], &vel_[4], &eff_[4]);
+  hardware_interface::JointStateHandle state_handle_5("joint5", &pos_[4], &vel_[4], &eff_[4]);
   jnt_state_interface.registerHandle(state_handle_5);
-  hardware_interface::JointStateHandle state_handle_6("arm6_joint", &pos_[5], &vel_[5], &eff_[5]);
+  hardware_interface::JointStateHandle state_handle_6("joint6", &pos_[5], &vel_[5], &eff_[5]);
   jnt_state_interface.registerHandle(state_handle_6);
   registerInterface(&jnt_state_interface);
 
-  hardware_interface::JointHandle eff_handle_1(jnt_state_interface.getHandle("arm1_joint"), &cmd_[0]);
+  hardware_interface::JointHandle eff_handle_1(jnt_state_interface.getHandle("joint1"), &cmd_[0]);
   jnt_position_interface.registerHandle(eff_handle_1);
-  hardware_interface::JointHandle eff_handle_2(jnt_state_interface.getHandle("arm2_joint"), &cmd_[1]);
+  hardware_interface::JointHandle eff_handle_2(jnt_state_interface.getHandle("joint2"), &cmd_[1]);
   jnt_position_interface.registerHandle(eff_handle_2);
-  hardware_interface::JointHandle eff_handle_3(jnt_state_interface.getHandle("arm3_joint"), &cmd_[2]);
+  hardware_interface::JointHandle eff_handle_3(jnt_state_interface.getHandle("joint3"), &cmd_[2]);
   jnt_position_interface.registerHandle(eff_handle_3);
-  hardware_interface::JointHandle eff_handle_4(jnt_state_interface.getHandle("arm4_joint"), &cmd_[3]);
+  hardware_interface::JointHandle eff_handle_4(jnt_state_interface.getHandle("joint4"), &cmd_[3]);
   jnt_position_interface.registerHandle(eff_handle_4);
-  hardware_interface::JointHandle eff_handle_5(jnt_state_interface.getHandle("arm5_joint"), &cmd_[4]);
+  hardware_interface::JointHandle eff_handle_5(jnt_state_interface.getHandle("joint5"), &cmd_[4]);
   jnt_position_interface.registerHandle(eff_handle_5);
-  hardware_interface::JointHandle eff_handle_6(jnt_state_interface.getHandle("arm6_joint"), &cmd_[5]);
+  hardware_interface::JointHandle eff_handle_6(jnt_state_interface.getHandle("joint6"), &cmd_[5]);
   jnt_position_interface.registerHandle(eff_handle_6);
   registerInterface(&jnt_position_interface);
 
